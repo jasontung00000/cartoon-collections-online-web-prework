28e7f70ce76f574a49064755ff49c609adca85d6
Jason Tung <jasontung123@yahoo.com>
2019-02-07 03:17:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves
  dwarves = ["Dopey", "Grumpy", "Bashful"]
- roll_call_dwarves(dwarves).each_with_index
+ roll_call_dwarves(dwarves) 
 end
 
 def summon_captain_planet# code an argument here
